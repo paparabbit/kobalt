617c62865d83458f5c5335b02a8f07b7234d2d93
leigh kayley <leigh@hoppermagic.com>
2017-09-25 19:48:51 +0100

    backlink can be disabled

--- src/views/partials/breadcrumb.blade.php
+++ src/views/partials/breadcrumb.blade.php
@@ -2,7 +2,11 @@
     @if ($depth == 1)
         <li class="active">List {{ str_plural($title) }}</li>
     @else
-        <li><a href="{{ $back_link }}">Back to {{  $back_title != '' ? str_plural($back_title) : str_plural($title) }}</a>
+        <li>
+                @if($back_link != null)
+                        <a href="{{ $back_link }}">Back to {{  $back_title != '' ? str_plural($back_title) : str_plural($title) }}</a>
+                @endif
+        </li>
         <li class="active">{{ $button_text. ' ' .$title }}</li>
     @endif
 </ol>
