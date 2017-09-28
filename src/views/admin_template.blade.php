958482cc04bb12723fe54470b35bd55e3fea2f74
leigh kayley <leigh@hoppermagic.com>
2017-09-28 09:17:13 +0100

    Don’t really need a footer with copyright

--- src/views/admin_template.blade.php
+++ src/views/admin_template.blade.php
@@ -71,14 +71,7 @@
 
 
     @yield('content')
-
-    <div class="container">
-        <div class="row footer">
-            <div class="col-sm-12">
-                <p id="externalButton">© Hoppermagic <?php echo date("Y"); ?></p>
-            </div>
-        </div>
-    </div>
+    
 
     <!-- JavaScripts -->
     {{--<script src="https://code.jquery.com/jquery-2.2.4.min.js"   integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44="   crossorigin="anonymous"></script>--}}
