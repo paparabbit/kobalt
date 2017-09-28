54469e11751d2bc72eb285bd8713789d1b801f32
leigh kayley <leigh@hoppermagic.com>
2017-09-28 10:14:43 +0100

    Mixing the less files as well as the js now

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,3 +13,12 @@ let mix = require('laravel-mix');
 
 mix
     .js('src/resources/src/js/app.js', 'src/resources/assets/admin.js')
+    .less('src/resources/src/less/app.less', 'src/resources/assets/admin.css')
+    .options({
+        postCss: [
+            require('autoprefixer')({
+                browsers: ["Last 2 versions", "Safari 8"],
+                cascade: false
+            })
+        ]
+    })
