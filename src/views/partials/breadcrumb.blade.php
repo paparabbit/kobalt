99ddd086ac826d08c7d24bcbcbae814decac841c
leigh kayley <leigh@hoppermagic.com>
2017-10-25 17:30:53 +0100

    Tweaking breadcrumb display

--- src/views/partials/breadcrumb.blade.php
+++ src/views/partials/breadcrumb.blade.php
@@ -2,11 +2,11 @@
     @if ($depth == 1)
         <li class="active">List {{ str_plural($title) }}</li>
     @else
-        <li>
-                @if($back_link != null)
-                        <a href="{{ $back_link }}">Back to {{  $back_title != '' ? str_plural($back_title) : str_plural($title) }}</a>
-                @endif
-        </li>
+        @if($back_link != null)
+            <li>
+                <a href="{{ $back_link }}">Back to {{  $back_title != '' ? str_plural($back_title) : str_plural($title) }}</a>
+            </li>
+        @endif
         <li class="active">{{ $button_text. ' ' .$title }}</li>
     @endif
 </ol>
