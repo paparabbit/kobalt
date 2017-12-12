16e542c016747c572d225f1989fa3a9fa451f2f1
leigh kayley <leigh@hoppermagic.com>
2017-12-12 16:15:06 +0000

    Whole bunch of stuff not needed for show

--- src/views/show.blade.php
+++ src/views/show.blade.php
@@ -13,14 +13,10 @@
 
             @include('kobalt::partials.page_header', ['title_text' => 'Show'])
 
-            {!! form_start($show_form) !!}
-            {{--{!! form_until($edit_form, 'save_field') !!}--}}
-
-            {{--!TODO Need to override the form until method to fix this properly--}}
+            {!! form($show_form) !!}
 
             <div class="row form-buttons">
                 <div class="col-sm-12">
-                    {!! form_rest($show_form) !!}
                     <a href="{{ $back_link }}" class="btn btn-info" id="back_button">Back</a>
 
                     @if (isset($delete_link))
@@ -28,9 +24,6 @@
                     @endif
                 </div>
             </div>
-
-            {!! form_end($show_form) !!}
-
 
         </div>
     </div>
