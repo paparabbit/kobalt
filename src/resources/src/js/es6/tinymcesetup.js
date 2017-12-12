06e88a0c8a1326d72f562164f2e9e6ad4fd116ff
leigh kayley <leigh@hoppermagic.com>
2017-12-12 18:45:41 +0000

    Tiny mce intro style

--- src/resources/src/js/es6/tinymcesetup.js
+++ src/resources/src/js/es6/tinymcesetup.js
@@ -4,15 +4,17 @@ export default class TinyMce {
     constructor(){
 
         tinymce.init({
-                 selector: '.rich-text',
-                 plugins: [
-                     'paste code, link'
-                 ],
-                 toolbar: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link',
-                 content_css: [
-                     '/css/admin.css',
-                 ],
-             });
+             selector: '.rich-text',
+             plugins: [
+                 'paste code, link'
+             ],
+             toolbar: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link',
+                style_formats: [
+                    { title: 'Intro copy', block: 'p', classes: 'intro-copy' },
+                ],
+                content_css: [
+                 '/css/admin.css',
+             ],
+         });
     }
-
 }
