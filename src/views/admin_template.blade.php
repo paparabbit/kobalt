c875f9b87781a5bfeaa0a83b55399e7bc93af71a
leigh kayley <leigh@hoppermagic.com>
2018-09-27 16:34:33 +0100

    Fixing font sizes and navbkg

--- src/views/admin_template.blade.php
+++ src/views/admin_template.blade.php
@@ -23,7 +23,7 @@
 
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
+        <nav class="navbar navbar-expand-md navbar-light bg-light">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', 'Laravel') }}
