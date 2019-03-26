e68c1bb541b46aff854258e9258d5b6da170bd7d
leigh kayley <leigh@hoppermagic.com>
2019-03-26 19:38:30 +0000

    fixing

--- src/resources/assets/admin.js
+++ src/resources/assets/admin.js
@@ -73080,8 +73080,8 @@ __webpack_require__(/*! event-emitter-es6 */ "./node_modules/event-emitter-es6/i
  */
 
 
-window.axios = __webpack_require__(/*! axios */ "./node_modules/axios/index.js");
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+window.axios = __webpack_require__(/*! axios */ "./node_modules/axios/index.js"); // window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
 /**
  * Next we will register the CSRF Token as a common header with Axios so that
  * all outgoing HTTP requests automatically have it attached. This is just
