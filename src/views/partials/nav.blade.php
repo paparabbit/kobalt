21178d16caaa6c3d076002968f7343361124293e
leigh kayley <leigh@hoppermagic.com>
2019-03-27 10:06:06 +0000

    Format of the admin nav needs to change a bit

--- src/views/partials/nav.blade.php
+++ src/views/partials/nav.blade.php
@@ -1,6 +1,6 @@
 <ul class="navbar-nav mr-auto">
-    {{--<li class="{{ ($active == 'overviewpage') ? 'active' : '' }}"><a href="{{ route('admin.overviewpage.index') }}">Overview pages</a></li>--}}
-    {{--<li class="{{ ($active == 'blog') ? 'active' : '' }}"><a href="{{ route('admin.blog.index') }}">Blog posts</a></li>--}}
-    {{--<li class="{{ ($active == 'work') ? 'active' : '' }}"><a href="{{ route('admin.work.index') }}">Projects</a></li>--}}
-    {{--<li class="{{ ($active == 'testimonials') ? 'active' : '' }}"><a href="{{ route('admin.testimonials.index') }}">Testimonials</a></li>--}}
+    {{--<li class="{{ ($active == 'overviewpage') ? 'active' : '' }}"><a href="{{ route('admin.overviewpage.index') }}" class="nav-link">Overview pages</a></li>--}}
+    {{--<li class="{{ ($active == 'blog') ? 'active' : '' }}"><a href="{{ route('admin.blog.index') }}" class="nav-link">Blog posts</a></li>--}}
+    {{--<li class="{{ ($active == 'work') ? 'active' : '' }}"><a href="{{ route('admin.work.index') }}" class="nav-link">Projects</a></li>--}}
+    {{--<li class="{{ ($active == 'testimonials') ? 'active' : '' }}"><a href="{{ route('admin.testimonials.index') }}" class="nav-link">Testimonials</a></li>--}}
 </ul>
