ea49cf43cb0b9d95a1444352eb6ad86e3db1de43
leigh kayley <leigh@hoppermagic.com>
2019-03-27 10:32:24 +0000

    We dont need sass?

--- src/resources/assets/admin.js
+++ src/resources/assets/admin.js
@@ -75794,8 +75794,7 @@ window.Popper = __webpack_require__(/*! popper.js */ "./node_modules/popper.js/d
 try {
   window.$ = window.jQuery = __webpack_require__(/*! jquery */ "./node_modules/jquery/dist/jquery.js");
 
-  __webpack_require__(/*! bootstrap */ "./node_modules/bootstrap/dist/js/bootstrap.js"); // require('bootstrap-sass')
-
+  __webpack_require__(/*! bootstrap */ "./node_modules/bootstrap/dist/js/bootstrap.js");
 } catch (e) {}
 /**
  * Vue is a modern JavaScript library for building interactive web interfaces
