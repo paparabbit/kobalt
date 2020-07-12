cab6d846c8e1945f7ecb3f8898cfe9a5d1d03d56
leigh kayley <leigh@hoppermagic.com>
2020-07-12 17:02:41 +0100

    removed str_plural and replaced with Str::plural

--- src/views/partials/breadcrumb.blade.php
+++ src/views/partials/breadcrumb.blade.php
@@ -1,10 +1,10 @@
 <ol class="breadcrumb bg-light">
     @if ($depth == 1)
-        <li class="breadcrumb-item active">List {{ str_plural($title) }}</li>
+        <li class="breadcrumb-item active">List {{ Str::plural($title) }}</li>
     @else
         @if($back_link != null)
             <li class="breadcrumb-item">
-                <a href="{{ $back_link }}">Back to {{  $back_title != '' ? str_plural($back_title) : str_plural($title) }}</a>
+                <a href="{{ $back_link }}">Back to {{  $back_title != '' ? Str::plural($back_title) : Str::plural($title) }}</a>
             </li>
         @endif
         <li class="breadcrumb-item active">{{ $button_text. ' ' .$title }}</li>
